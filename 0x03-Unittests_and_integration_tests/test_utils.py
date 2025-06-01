90eaa1d3395fe1e7ab561bb35f459c9a6cf6903c
nezer1 <nyemiteijesse@gmail.com>
2025-06-01 23:39:50 +0000

    finshing up'

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -55,7 +55,7 @@ class TestGetJson(unittest.TestCase):
         mock_response = Mock()
         mock_response.json.return_value = test_payload
 
-        with patch("requests.get", return_value=mock_response) as mock_get:
+        with patch("utils.requests.get", return_value=mock_response) as mock_get:
             result = get_json(test_url)
             self.assertEqual(result, test_payload)
             mock_get.assert_called_once_with(test_url)

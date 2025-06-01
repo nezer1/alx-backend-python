9b5040f9d2ca7bac6f6b867f2d1cdd5e575d4aaf
nezer1 <nyemiteijesse@gmail.com>
2025-06-01 07:29:00 +0000

    finshing up'

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+class ExecuteQuery:
+    def __init__(self, db_path, query, params=None):
+        self.db_path = db_path
+        self.query = query
+        self.params = params or ()
+        self.conn = None
+        self.result = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_path)
+        cursor = self.conn.cursor()
+        cursor.execute(self.query, self.params)
+        self.result = cursor.fetchall()
+        return self.result
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+        if exc_type:
+            print("An error occurred:", exc_val)
+        # Returning False lets exceptions propagate
+        return False

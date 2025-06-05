f9a4d1547a233435d711efb7dd37844273268fa3
nezer1 <nyemiteijesse@gmail.com>
2025-06-05 13:06:37 +0000

    finishing up

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,8 +5,8 @@ import unittest
 from typing import Mapping, Sequence, Any
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
-#from unittest.mock import patch, Mock
-#from client import GithubOrgClient
+from unittest.mock import patch, Mock
+from client import GithubOrgClient
 
 
 
@@ -28,22 +28,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map with various nested structures."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    # @parameterized.expand([
-    #     ({}, ("a",), "a"),
-    #     ({"a": 1}, ("a", "b"), "b"),
-    # ])
-    # def test_access_nested_map_exception(
-    #         self,
-    #         nested_map: Mapping,
-    #         path: Sequence[str],
-    #         missing_key: str
-    # ) -> None:
-    #     """Test that access_nested_map raises KeyError for missing keys
-    #     and that the exception message is the missing key."""
-    #     with self.assertRaises(KeyError) as context:
-    #         access_nested_map(nested_map, path)
-    #     self.assertEqual(str(context.exception), repr(missing_key)) 
-    #     #key error exception internally uses repr
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence[str],
+            missing_key: str
+    ) -> None:
+        """Test that access_nested_map raises KeyError for missing keys
+        and that the exception message is the missing key."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), repr(missing_key)) 
+        #key error exception internally uses repr
 
 
 

8e8d90544ba56d05eed342e0eacf408f67786d9a
Jesse Nyemitei <43499607+nezer1@users.noreply.github.com>
2025-06-06 10:19:55 +0000

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,21 +27,21 @@ class TestAccessNestedMap(unittest.TestCase):
      """Test access_nested_map with various nested structures."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
     
-    @parameterized.expand([
-        ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), "b"),
-    ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map: Mapping,
-            path: Sequence[str],
-            missing_key: str) -> None:
-        """Test that access_nested_map raises KeyError for missing keys
-        and that the exception message is the missing key."""
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), repr(missing_key)) 
-        #key error exception internally uses repr
+    # @parameterized.expand([
+    #     ({}, ("a",), "a"),
+    #     ({"a": 1}, ("a", "b"), "b"),
+    # ])
+    # def test_access_nested_map_exception(
+    #         self,
+    #         nested_map: Mapping,
+    #         path: Sequence[str],
+    #         missing_key: str) -> None:
+    #     """Test that access_nested_map raises KeyError for missing keys
+    #     and that the exception message is the missing key."""
+    #     with self.assertRaises(KeyError) as context:
+    #         access_nested_map(nested_map, path)
+    #     self.assertEqual(str(context.exception), repr(missing_key)) 
+    #     #key error exception internally uses repr
 
 
 

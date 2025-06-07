abf08248dd71cd4248079843cb99c8106b3aecc4
Jesse Nyemitei <43499607+nezer1@users.noreply.github.com>
2025-06-07 08:55:13 +0000

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,7 +27,8 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = expected_payload
 
         client = GithubOrgClient(org_name)
-        self.assertEqual(client.org, expected_payload)
+        output = client.org
+        self.assertEqual(output, expected_payload)
         
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
